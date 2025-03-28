c455c2bc94349769c49c29a03b170c024bd9289a
Gopi Kumar <gopikumar764482@gmail.com>
2025-03-29 02:07:44 +0530

    footer path correction

--- src/AppLayout/Layout.jsx
+++ src/AppLayout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
-import Footer from './footer'
+import Footer from "../AppLayout/Footer"
 
 const Layout = () => {
   return (
